8c4e28975bede1bce06582c33d5b40980d43225b
POTTELLA SAI PREETHI <54733453+saipreethi9-design@users.noreply.github.com>
2023-10-10 15:06:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,6 @@
 provider "google" {
   project = var.project
   region  = var.region
-  zone    = var.zone
-}
-
-resource "google_project_service" "api" {
-  for_each           = toset(var.service_list)
-  disable_on_destroy = false
-  service            = each.value
-}
-
-resource "google_compute_firewall" "web" {
-  name          = "web-access"
-  network       = "default"
-  source_ranges = var.source_ranges
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "443"]
-  }
 }
 
 resource "google_compute_instance" "web_server" {
@@ -35,7 +17,4 @@ resource "google_compute_instance" "web_server" {
     network = "default"
     access_config {}
   }
-
-  metadata_startup_script = "sudo apt update -y \n sudo apt install apache2 -y \n echo \"<h2>GCP WebServer Using Terraform!<h2>\"   >  /var/www/html/index.html \nsudo systemctl restart apache2 \n"
-  depends_on              = [google_project_service.api, google_compute_firewall.web]
 }
