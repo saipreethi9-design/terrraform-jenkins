63c6742dcf0b964b6f939b6cd383189495d25cde
POTTELLA SAI PREETHI <54733453+saipreethi9-design@users.noreply.github.com>
2023-10-10 15:10:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,22 +11,6 @@ variable "region" {
 variable "zone" {
   description = "The Google Cloud zone"
   default     = "us-east1-b"
-
-}
-
-variable "service_list" {
-  description = "A list of Google Cloud services to enable"
-  type        = list(string)
-  default = [
-    "cloudresourcemanager.googleapis.com",
-    "compute.googleapis.com",
-  ]
-}
-
-variable "source_ranges" {
-  description = "The source IP ranges allowed for firewall rules"
-  type        = list(string)
-  default     = ["0.0.0.0/0"]
 }
 
 variable "machine_type" {
